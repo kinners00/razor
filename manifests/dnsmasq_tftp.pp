0672f1de8b56907ba96722ea543fb6b488cee324
Administrator <admin@example.com>
2017-11-17 12:51:57 +0000

    Add new file

---
+++ manifests/dnsmasq_tftp.pp
@@ -0,0 +1,44 @@
+# Class: razor::dnsmasq_tftp
+# Installs dnsmasq and configures it for TFTP only
+#
+class razor::dnsmasq_tftp {
+  # resources
+  $razor_content = "# iPXE sets option 175, mark it for network IPXEBOOT
+dhcp-match=IPXEBOOT,175
+dhcp-boot=net:IPXEBOOT,bootstrap.ipxe
+dhcp-boot=undionly-20140116.kpxe
+# TFTP setup
+enable-tftp
+tftp-root=/var/lib/tftpboot"
+
+  file { '/var/lib/tftpboot':
+    ensure => 'directory',
+    mode   => '0655',
+  }
+  -> package { 'dnsmasq':
+    ensure => 'present',
+  }
+  -> file { '/etc/dnsmasq.d/razor':
+    ensure  => 'file',
+    content => $razor_content,
+  }
+  ~> service { 'dnsmasq':
+    ensure => 'running',
+  }
+
+  package { 'policycoreutils-python':
+    ensure  => 'present',
+    require => File['/var/lib/tftpboot'],
+  }
+  -> exec { 'Define SELinux tftpdir_t file type for tftpboot folder':
+    command => '/usr/sbin/semanage fcontext -a -t tftpdir_t "/var/lib/tftpboot(/.*)?"',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    unless  => 'ls -Z /var/lib | grep tftpboot | grep tftpdir_t',
+  }
+  -> exec { 'Apply SELinux tftpdir_t file type to tftpboot folder hierarchy':
+    before  => Service['dnsmasq'],
+    command => '/sbin/restorecon -R -v /var/lib/tftpboot',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    unless  => 'ls -Z /var/lib | grep tftpboot | grep tftpdir_t',
+  }
+}
