5833f23d851086cf264ad924af0d8ee3d92311d9
Administrator <admin@example.com>
2017-11-21 10:50:54 +0000

    Update bootstrap.pp

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -10,6 +10,11 @@ class razor::bootstrap {
     ensure => installed,
   }
   
+  host { 'razor.inf.puppet.vm':
+    before => Exec['Download bootstrap.ipxe from Razor server'],
+    ip => $facts['ipaddress'],
+  }
+  
   exec { 'Download bootstrap.ipxe from Razor server':
     command => "/usr/bin/wget -k https://${facts['fqdn']}:8151/api/microkernel/bootstrap?nic_max=1&http_port=8150 -O /var/lib/tftpboot/bootstrap.ipxe",
     creates => '/var/lib/tftpboot/bootstrap.ipxe',
