dbc8283453a985ac88553ab31036ba1dc92eba84
Administrator <admin@example.com>
2017-11-21 10:12:03 +0000

    Update bootstrap.pp

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -5,8 +5,13 @@ class razor::bootstrap {
   # resources
   require pe_razor
   
-  file { '/var/lib/tftpboot/bootstrap.ipxe':
-    ensure => 'file',
-    source => "https://$facts['fqdn']:8151/api/microkernel/bootstrap?nic_max=1&http_port=8150",
+  package { 'wget':
+    before => Exec['Download bootstrap.ipxe from Razor server'],
+    ensure => installed,
+  }
+  
+  exec { 'Download bootstrap.ipxe from Razor server':
+    command => "wget https://$facts['fqdn']:8151/api/microkernel/bootstrap?nic_max=1&http_port=8150 -O /var/lib/tftpboot/bootstrap.ipxe",
+    creates => '/var/lib/tftpboot/bootstrap.ipxe',
   }
 }
