90abc002f7454078eb91c14bf0bcd3f73de87ae0
Administrator <admin@example.com>
2017-11-21 10:22:11 +0000

    Update bootstrap.pp

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -5,13 +5,15 @@ class razor::bootstrap {
   # resources
   require pe_razor
   
-  package { 'wget':
-    before => Exec['Download bootstrap.ipxe from Razor server'],
-    ensure => installed,
-  }
+  #package { 'wget':
+  #  before => Exec['Download bootstrap.ipxe from Razor server'],
+  #  ensure => installed,
+  #}
   
-  exec { 'Download bootstrap.ipxe from Razor server':
-    command => "wget https://${facts['fqdn']}:8151/api/microkernel/bootstrap?nic_max=1&http_port=8150 -O /var/lib/tftpboot/bootstrap.ipxe",
-    creates => '/var/lib/tftpboot/bootstrap.ipxe',
-  }
+  #exec { 'Download bootstrap.ipxe from Razor server':
+  #  command => "wget https://${facts['fqdn']}:8151/api/microkernel/bootstrap?nic_max=1&http_port=8150 -O /var/lib/tftpboot/bootstrap.ipxe",
+  #  creates => '/var/lib/tftpboot/bootstrap.ipxe',
+  #}
+  
+  
 }
