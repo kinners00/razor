d334338e3a11bd002ef43f7695ba5e0a33802ac2
Administrator <admin@example.com>
2017-12-04 09:36:30 +0000

    Update dnsmasq_tftp.pp

--- manifests/dnsmasq_tftp.pp
+++ manifests/dnsmasq_tftp.pp
@@ -13,11 +13,13 @@ tftp-root=/var/lib/tftpboot"
 
   file { '/var/lib/tftpboot':
     ensure => 'directory',
+    seltype => 'tftpdir_rw_t',
     mode   => '0655',
   }
   -> file { '/var/lib/tftpboot/undionly-20140116.kpxe':
-    ensure => 'file',
-    source => 'https://s3.amazonaws.com/pe-razor-resources/undionly-20140116.kpxe',
+    ensure  => 'file',
+    seltype => 'tftpdir_rw_t',
+    source  => 'https://s3.amazonaws.com/pe-razor-resources/undionly-20140116.kpxe',
   }
   -> package { 'dnsmasq':
     ensure => 'present',
