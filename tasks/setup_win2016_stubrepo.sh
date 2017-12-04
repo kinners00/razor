35a2fe0c4a9a4bbab03fa76fdb71ea8066be00e9
Administrator <admin@example.com>
2017-12-04 17:28:35 +0000

    Update setup_win2016_stubrepo.sh

--- tasks/setup_win2016_stubrepo.sh
+++ tasks/setup_win2016_stubrepo.sh
@@ -9,7 +9,7 @@ if [ -e /root/SW_DVD9_Win_Svr_STD_Core_and_DataCtr_Core_2016_64Bit_English_-2_ML
   mount -o loop /root/SW_DVD9_Win_Svr_STD_Core_and_DataCtr_Core_2016_64Bit_English_-2_MLF_X21-22843.ISO /mnt
   cp -pr /mnt/* /opt/puppetlabs/server/data/razor-server/repo/win2016
   umount /mnt
-  chown -R pe-razor: /opt/puppetlabs/server/data/razor-server/repo/win2016
+  chown -R pe-razor:pe-razor /opt/puppetlabs/server/data/razor-server/repo/win2016
   echo "Done!"
  else
   echo "Windows 2016 ISO not found, canceling..."
