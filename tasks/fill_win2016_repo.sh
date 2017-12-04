32bcdbe89fcc9d70dce3fe597c0d0541aec99be3
Administrator <admin@example.com>
2017-12-04 17:20:26 +0000

    Delete fill_win2016_repo.sh

--- tasks/fill_win2016_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mount -o loop /root/SW_DVD9_Win_Svr_STD_Core_and_DataCtr_Core_2016_64Bit_English_-2_MLF_X21-22843.ISO /mnt
-cp -pr /mnt/* /opt/puppetlabs/server/data/razor-server/repo/win2016
-umount /mnt
-chown -R pe-razor: /opt/puppetlabs/server/data/razor-server/repo/win2016
