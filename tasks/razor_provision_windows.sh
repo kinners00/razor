a3c6d9251012c5e52f06e7a0f20e17ecc4b96bb8
Administrator <admin@example.com>
2017-12-06 15:08:46 +0000

    Update razor_add_tag_windows.sh

--- tasks/razor_provision_windows.sh
+++ tasks/razor_provision_windows.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+razor register-node --hw-info net0=$PT_mac --installed false | grep name | gawk
+
+
 razor create-tag --name $PT_name --rule '["=", ["fact", "macaddress"], "'$PT_mac'"]'
 razor add-policy-tag --name $PT_policy --tag $PT_name
 razor tags $PT_name nodes
