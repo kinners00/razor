2541bffe3f97419b121fd9dd71ccfb5aaafd8a30
Administrator <admin@example.com>
2017-12-06 15:22:44 +0000

    Update razor_provision_windows.sh

--- tasks/razor_provision_windows.sh
+++ tasks/razor_provision_windows.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-razor register-node --hw-info net0=$PT_mac --installed false | grep name | gawk
-
-
-razor create-tag --name $PT_name --rule '["=", ["fact", "macaddress"], "'$PT_mac'"]'
+nodename=`razor register-node --hw-info net0=$PT_mac --installed false | grep name | awk -F':' '{ gsub(/ /, "", $0); print $2 }'`
+razor update-node-metadata --node $nodename --key hostname --value $PT_fqdn
+razor create-tag --name $PT_fqdn --rule '["=", ["fact", "macaddress"], "'$PT_mac'"]'
 razor add-policy-tag --name $PT_policy --tag $PT_name
-razor tags $PT_name nodes
+razor tags $PT_fqdn nodes
