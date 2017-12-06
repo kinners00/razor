498d66fa72867a6a00d7982c2083b738c6d039b1
Administrator <admin@example.com>
2017-12-06 15:27:31 +0000

    Update provision_windows.sh

--- tasks/provision_windows.sh
+++ tasks/provision_windows.sh
@@ -2,5 +2,5 @@
 nodename=`razor register-node --hw-info net0=$PT_mac --installed false | grep name | awk -F':' '{ gsub(/ /, "", $0); print $2 }'`
 razor update-node-metadata --node $nodename --key hostname --value $PT_fqdn
 razor create-tag --name $PT_fqdn --rule '["=", ["fact", "macaddress"], "'$PT_mac'"]'
-razor add-policy-tag --name $PT_policy --tag $PT_name
+razor add-policy-tag --name $PT_policy --tag $PT_fqdn
 razor tags $PT_fqdn nodes
