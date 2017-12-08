cc1c55917f89316e627b1e740d46f589cdaea9bb
Administrator <admin@example.com>
2017-12-08 12:05:04 +0000

    Update provision_windows.sh

--- tasks/provision_windows.sh
+++ tasks/provision_windows.sh
@@ -13,19 +13,20 @@ fi
 razor update-node-metadata --node $razornode --key hostname --value $PT_fqdn
 
 # Update the tag rule or create the tag if it doesn't exist
-if [ -z `razor tags | grep $PT_policy`]
+tagname = ${PT_policy}_tag
+if [ -z `razor tags | grep $tagname`]
 then
-  # Tag doesn't exist, so create it
-  razor create-tag --name $PT_policy --rule '["has_macaddress", "'$PT_mac'"]'
+  # Tag doesn't exist, so create it and add it to the policy
+  razor create-tag --name $tagname --rule '["has_macaddress", "'$PT_mac'"]'
+  razor add-policy-tag --name $PT_policy --tag $tagname
 else
-  # Tag already exists, update the tag rule
-  rule_macs=``
-  razor update-tag-rule --name $PT_policy --force --rule '["has_macaddress", "'$rule_macs'"]'
+  # Tag already exists, check if it uses the correct rule
+  if [ -z `razor tags $tagname | grep "rule: \[\"has_macaddress\", " ]
+  then
+    echo "The razor tag rule $tagname is not configured to exclusively use has_macaddress! Canceling modification of the rule..."
+    exit 1
+  else
+    # Tag rule is correct, update the rule
+    new_rule_macs=`razor tags $tagname | grep "rule: \[\"has_macaddress\", " | awk -F, ' { $NF = substr($NF, 1, length($NF)-1); for (i=2; i<NF; i++) printf $i ", "; print $NF, ",\"'$PT_mac'\"]" }'`
+    razor update-tag-rule --name $tagname --force --rule '["has_macaddress", "'$new_rule_macs'"]'
 fi
-
-
-
-
-
-razor add-policy-tag --name $PT_policy --tag $PT_fqdn
-razor tags $PT_fqdn policies
